bea9a7467bf55122a21fc328be3c52305b495920
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-06-18 00:17:30 +0900

    refactor: config 삭제

--- config/session.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto')
-
-module.exports = {
-    secret: crypto.randomBytes(32).toString('hex'),
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-}
